48f0923b4cc0bb66919897a62ab9e309c1a504c7
TheDarkUFO <life.darkufo@gmail.com>
2020-06-16 17:05:55 +0500

    Добавил обработку файла json

--- HTML/Store Site Example/js/lotsplacer.js
+++ HTML/Store Site Example/js/lotsplacer.js
@@ -1,32 +1,14 @@
 $.ajax({
     url: './js/json/paintings.json',
-    success: function(data) {
+    success: function(data)
+    {
+        $object = "<div class=\"lot\">";
+        data.forEach(element => {
+            $title = element["title"];
+            $path = element["path"];
+            $object += "<a href=\"$path\" title=\"$title\"></a>";
+            $object += "</div>";
+        });
        console.log(data); // Выведет содержимое файла в консоль
     }
 });
-
-// if (window.XMLHttpRequest)
-// {
-//     xmlhttp = new XMLHttpRequest();
-// }
-// else
-// {// code for IE6, IE5
-//     xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-// }
-// xmlhttp.open("GET","./js/json/paintings.json", false);
-// xmlhttp.send();
-// xmlDoc=xmlhttp.responseText;
-
-
-//     // $json = file_get_contents("./json/paintings.json");
-//     // $data = json_decode(json, true);
-//     // alert(1);
-//     // $object = "<div class=\"lot\">";
-//     // foreach($data as $value)) {
-        
-//     //     $title = $value["title"];
-//     //     $path = $value["path"];
-//     //     $object .= "<a href=\"$path\" title=\"$title\"></a>
-//     // }
-//     //$object .= "</div>"
-// });
