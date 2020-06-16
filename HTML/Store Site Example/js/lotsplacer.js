a99b21325204454097b26d9255d742f791c5f2ee
TheDarkUFO <life.darkufo@gmail.com>
2020-06-16 17:40:07 +0500

    Добавил пункт стартовой стоимости и вывод изображения

--- HTML/Store Site Example/js/lotsplacer.js
+++ HTML/Store Site Example/js/lotsplacer.js
@@ -8,7 +8,11 @@ $.ajax({
             $path = $value["path"];
             $title = $value["title"];
             $desc = $value["desc"];
+            $img = $value["img"];
+            $start_cost = $value["start_cost"];
             $object += "<a href=\"" + $path + "\" title=\"" + $title + "\">" + $title + "</a>";
+            $object += "<img src=\"" + $img + "\" alt=\"" + $title + "\"/>";
+            $object += "<span>Стартовая стоимость: " + $start_cost + "</span>";
             $object += "</div>";
         });
         $object += "</div>";
